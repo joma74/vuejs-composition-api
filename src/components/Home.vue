c2d18b8a399e50d3acc243bd6669f35f7155610d
joma74 <dev-mails@gmx.net>
2021-10-05 00:10:54 +0200

    feat: Add Navbar and extract suspense into Home

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template
+  ><suspense
+    ><template #default><timeline /></template
+    ><template #fallback><spinner /></template></suspense
+></template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core"
+import Timeline from "@/components/Timeline.vue"
+import Spinner from "@/components/Spinner.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Timeline,
+    Spinner,
+  },
+})
+</script>
