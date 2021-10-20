f90c4382bfd9054a0c450b1240870b2d00391863
joma74 <dev-mails@gmx.net>
2021-10-21 00:38:51 +0200

    test: Added test for NavBar.vue - with teleport - with form

--- tests/unit/NavBar.spec.ts
+++ tests/unit/NavBar.spec.ts
@@ -1,5 +1,6 @@
-import { mount, flushPromises } from "@vue/test-utils"
+import { mount, flushPromises, DOMWrapper } from "@vue/test-utils"
 import Navbar from "@/components/Navbar.vue"
+import Signup from "@/components/Signup.vue"
 import { store } from "@/store"
 import { spyOnHandler, expectNoErrorOrWarnOccured } from "./jest.setup"
 
@@ -8,12 +9,24 @@ describe("Navbar", () => {
     let errorSpy = jest.fn()
     let warnSpy = jest.fn()
 
+    const el = document.createElement("div")
+    el.id = "modal"
+    document.body.appendChild(el)
+
     const wrapper = mount(
       Navbar,
       spyOnHandler(
         {
+          // else the Nabar component is only rendered as a virual dom
+          attachTo: document.body,
           global: {
             plugins: [store],
+            components: {
+              // Fixes "Failed to resolve component: router-link"
+              RouterLink: {
+                template: `<div></div>`,
+              },
+            },
           },
         },
         errorSpy,
@@ -21,6 +34,25 @@ describe("Navbar", () => {
       ),
     )
 
+    // does not work -> wrapper.get("form")
+    const form = wrapper.getComponent(Signup)
+
+    expect(document.body.outerHTML).toContain("This value is required.")
+
+    let usernameInput = form.get("[name='Username']")
+    await usernameInput.setValue("Username")
+    //
+    let passwordInput = form.get("[name='Password']")
+    await passwordInput.setValue("1")
+    //
+    expect(document.body.outerHTML).toContain(
+      "This value must be between 5 and 25.",
+    )
+    await passwordInput.setValue("12345678901")
+    //
+    expect(document.body.outerHTML).not.toContain("This value")
+    //
+    await form.trigger("submit.prevent")
     //
     expectNoErrorOrWarnOccured(errorSpy, warnSpy)
     done()
