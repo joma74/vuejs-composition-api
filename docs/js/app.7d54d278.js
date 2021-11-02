708796bedad813e8037ac6d5bef22701d376850a
joma74 <dev-mails@gmx.net>
2021-11-02 23:42:55 +0100

    chore: Setup production build for delivery via github homepage

---
+++ docs/js/app.7d54d278.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71dcda32f2e740d5f11a5eba0e50e21073474aafec735c9259a002ce1dbb7386
+size 27918
---
+++ docs/js/chunk-vendors.f77b53cb.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c223d3b49eebf368b3230d73eec3090fd1ef5d5cb543c5e9a14364d5f27c4cc
+size 1331583
